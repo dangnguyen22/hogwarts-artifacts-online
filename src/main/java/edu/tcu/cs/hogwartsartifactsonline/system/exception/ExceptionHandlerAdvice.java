77e4c4d70391b4647da1427cf9276d1fb3728d2d
Michael Nguyen <dang.nguyen@tcu.edu>
2024-05-18 11:46:17 -0500

    Update the Exception Handler Advice class so that we can handle the insufficient authentication exception.

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
@@ -5,6 +5,7 @@ import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.validation.FieldError;
@@ -44,6 +45,11 @@ public class ExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     Result handleAuthenticationException(Exception ex){
         return new Result(false, StatusCode.UNAUTHORIZED,"username or password is incorrect", ex.getMessage());
+    }
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleInsufficientAuthenticationExeception(InsufficientAuthenticationException ex){
+        return new Result(false, StatusCode.UNAUTHORIZED,"login credentials are missing.", ex.getMessage());
     }
 
     @ExceptionHandler(AccountStatusException.class)
